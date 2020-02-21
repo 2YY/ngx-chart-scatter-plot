9186a618881c33843c821a88514e0c8dbae65ae8
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-02-22 01:56:30 +0900

    Revert ":fire: Remove unnecessary service"
    This reverts commit 296bd60526bd2c9fd355519020239a4a22694502.

--- projects/lib-ngx-chart-scatter-plot/src/public-api.ts
+++ projects/lib-ngx-chart-scatter-plot/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of lib-ngx-chart-scatter-plot
  */
 
+export * from './lib/lib-ngx-chart-scatter-plot.service';
 export * from './lib/lib-ngx-chart-scatter-plot.component';
 export * from './lib/lib-ngx-chart-scatter-plot.module';
