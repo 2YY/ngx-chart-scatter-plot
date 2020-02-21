296bd60526bd2c9fd355519020239a4a22694502
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-02-22 01:56:30 +0900

    :fire: Remove unnecessary service

--- projects/lib-ngx-chart-scatter-plot/src/public-api.ts
+++ projects/lib-ngx-chart-scatter-plot/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of lib-ngx-chart-scatter-plot
  */
 
-export * from './lib/lib-ngx-chart-scatter-plot.service';
 export * from './lib/lib-ngx-chart-scatter-plot.component';
 export * from './lib/lib-ngx-chart-scatter-plot.module';
