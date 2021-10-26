278d9acc8a3be808a6cd98f941c49a5ac8efa28a
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2021-10-27 08:54:03 +0900

    :arrow_up: ng update @angular/core @angular/cli

--- projects/ngx-chart-scatter-plot/src/lib/ngx-chart-scatter-plot.component.spec.ts
+++ projects/ngx-chart-scatter-plot/src/lib/ngx-chart-scatter-plot.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {NgxChartScatterPlotComponent} from './ngx-chart-scatter-plot.component';
 
@@ -6,7 +6,7 @@ describe('NgxChartScatterPlotComponent', () => {
   let component: NgxChartScatterPlotComponent;
   let fixture: ComponentFixture<NgxChartScatterPlotComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [NgxChartScatterPlotComponent]
     })
